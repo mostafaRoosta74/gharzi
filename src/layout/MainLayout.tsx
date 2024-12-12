4665c3470b19709a7f650b1663d0b28dbfffc3b1
Mostafa Roosta <mostafaroosta74@gmail.com>
2024-12-12 22:08:07 +0330

    fix: fix login bug

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,16 +3,17 @@ import {Box, CircularProgress} from "@mui/material";
 import BottomNavigate from "./component/BottomNavigate";
 import {TopAppBar} from "./component/TopAppBar";
 import {useEffect} from "react";
+import { fixToken } from "../utils/utils";
 
 export const MainLayout = ()=> {
 
     if (!window.localStorage.getItem("token")){
-        window.location.pathname = "/login"
+        window.location.pathname = "/login";
         return <Box sx={{display:"flex",alignItems:"center",justifyContent:"center",width:"100vw",height:"100vh"}}>
             <CircularProgress/>
         </Box>
     }else{
-
+        fixToken(window.localStorage.getItem("token") || "");
         return (
             <Box>
                 <TopAppBar/>
